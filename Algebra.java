3cb0db956e251da23de1b70e8db33a1b09e037ca
elayhakim <elay.hakim@post.runi.ac.il>
2025-11-14 14:22:52 +0200

    Algebra

--- Algebra.java
+++ Algebra.java
@@ -27,8 +27,10 @@ public class Algebra {
 	public static int plus(int x1, int x2) {
 		int x=x1;
 		if(x2<0){
-			int a = minus(x1, minus(0,x2));
-			return a;
+			for(int i=0; i<x2; i++){
+			x--;
+		}
+		return x;
 		}else{
 		for(int i=0; i<x2; i++){
 			x++;
@@ -41,8 +43,10 @@ public class Algebra {
 	public static int minus(int x1, int x2) {
 		int x = x1;
 		if(x2<0){
-			int a = plus(x1, minus(0,x2));
-			return a;
+			for(int i=0; i<x2; i++){
+			x++;
+		}
+		return x;
 		}else{
 		for(int i=0; i<x2; i++){
 			x--;
@@ -53,29 +57,38 @@ public class Algebra {
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
+		int sighn = 1;
+		int x3=0;
+		int x4=0;
 		if(x1 == 0 || x2 ==0){
 			return 0;
 		}
 		int x=0;
 		if(x1 > 0 && x2 > 0){
-		for(int i=0; i<x2; i++){
-			x= plus(x, x1);
-		}
-		return x;
+			x3=x1;
+			x4=x2;
+			sighn = 1;
 		}else if (x1 < 0 && x2 < 0) {
-			for(int i=0; i< minus(0,x2); i++){
-				x= plus(x, minus(0,x1));
-			}
+			x3 = minus(0,x1);
+			x4 = minus(0,x2);
+			sighn =1;
 		}else if (x1 < 0) {
-			for(int i=0; i< x2; i++){
-				x= plus(x, minus(0,x1));
-			}
+			x3 = minus(0,x1);
+			x4= x2;
+			sighn = -1;
 		}else{
-			for(int i=0; i< minus(0,x2); i++){
-				x= plus(x, x1);
-			}
+			x3= x1;
+			x4 = minus(0,x2);
+			sighn = -1;
 		}
-		return x;
+		for(int i=0; i<x4; i++){
+				x= plus(x, x3);
+			}
+		if(sighn>0){
+			return x;
+		}else{
+			return (minus(0, x));
+		}
 	}
 
 	// Returns x^n (for n >= 0)
